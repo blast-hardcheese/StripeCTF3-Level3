9221c5819c10337e1e7ee7fcf122c7cc09fcf3b3
Devon Stewart <blast@hardchee.se>
2014-01-29 10:36:09 -0800

    Use a Broker to limit filesystem accesses

--- src/main/scala/com/stripe/ctf/instantcodesearch/Indexer.scala
+++ src/main/scala/com/stripe/ctf/instantcodesearch/Indexer.scala
@@ -6,16 +6,35 @@ import java.nio.file._
 import java.nio.charset._
 import java.nio.file.attribute.BasicFileAttributes
 
+import com.twitter.concurrent.Broker
+import com.twitter.util.{Future, FuturePool}
 
 /*
   Indexer traverses all files on the disk, maintaining a map of file path -> trigram -> Set[line numbers]
 */
 
 class Indexer {
+  val indexBroker = new Broker[(String, String)]()
   def isIndexed = true
 
+  def buildProcessor(id: Int) {
+    indexBroker.recv.sync() map { case (abspath: String, relpath: String) =>
+      FuturePool.unboundedPool {
+        println(s"$id: Index $abspath")
+      } map { _ => buildProcessor(id) }
+    }
+  }
+
+  (1 to 3).map(buildProcessor)
+
+  val decoder = Charset.forName("UTF-8").newDecoder()
+  decoder onMalformedInput CodingErrorAction.REPORT
+  decoder onUnmappableCharacter CodingErrorAction.REPORT
+
   def indexFile(abspath: String, relpath: String): Indexer = {
-    println("Stubbed")
+
+    indexBroker !! (abspath, relpath)
+
     return this
   }
 }
