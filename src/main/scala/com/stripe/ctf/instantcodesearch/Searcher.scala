3fac82bcd3e2a0568ed731919a4b259896a929ed
Devon Stewart <blast@hardchee.se>
2014-01-29 11:27:30 -0800

    Rewrite Searcher to interface directly with Indexer

--- src/main/scala/com/stripe/ctf/instantcodesearch/Searcher.scala
+++ src/main/scala/com/stripe/ctf/instantcodesearch/Searcher.scala
@@ -9,39 +9,18 @@ abstract class SearchResult
 case class Match(path: String, line: Int) extends SearchResult
 case class Done() extends SearchResult
 
-class Searcher(indexPath : String)  {
-  val index : Index = readIndex(indexPath)
-  val root = FileSystems.getDefault().getPath(index.path)
-
+class Searcher(indexer: Indexer)  {
   def search(needle : String, b : Broker[SearchResult]) = {
-    for (path <- index.files) {
-      for (m <- tryPath(path, needle)) {
-        b !! m
-      }
-    }
+    val needlegrams = needle.sliding(3).toSet
+
+    for {
+      (fngrams, relpath) <- indexer.ngrams
+      if((needlegrams intersect fngrams) == needlegrams)
+      (line, no) <- indexer.files(relpath).zipWithIndex
+      if(line contains needle)
+    } yield b !! Match(relpath, no+1)
 
     b !! new Done()
   }
 
-  def tryPath(path: String, needle: String) : Iterable[SearchResult] = {
-    try {
-      val text : String = slurp(root.resolve(path))
-      if (text.contains(needle)) {
-        var line = 0
-        return text.split("\n").zipWithIndex.
-          filter { case (l,n) => l.contains(needle) }.
-          map { case (l,n) => new Match(path, n+1) }
-      }
-    } catch {
-      case e: IOException => {
-        return Nil
-      }
-    }
-
-    return Nil
-  }
-
-  def readIndex(path: String) : Index = {
-    new ObjectInputStream(new FileInputStream(new File(path))).readObject.asInstanceOf[Index]
-  }
 }
